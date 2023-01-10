80e91ceb66c4ba0be26ff892e98eb2635370bd97
caiming01 <caiming01@qianxin.com>
2023-01-10 16:05:56 +0800

    feature:body: 新增 Jpa

--- spring-mybatis/src/main/java/com/caijuan/springmybatis/SpringMybatisApplication.java
+++ spring-mybatis/src/main/java/com/caijuan/springmybatis/SpringMybatisApplication.java
@@ -2,7 +2,10 @@ package com.caijuan.springmybatis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+// step 2 : 开启定时调度
+@EnableScheduling
 @SpringBootApplication
 public class SpringMybatisApplication {
 
