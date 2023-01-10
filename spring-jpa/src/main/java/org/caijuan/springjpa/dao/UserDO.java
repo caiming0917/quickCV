dc621d4c53229f3f95bba926366a5e7e30dc5fe2
caiming01 <caiming01@qianxin.com>
2023-01-10 20:43:32 +0800

    feature:body: 新增 Jpa

--- spring-jpa/src/main/java/org/caijuan/springjpa/dao/UserDO.java
+++ spring-jpa/src/main/java/org/caijuan/springjpa/dao/UserDO.java
@@ -2,22 +2,28 @@ package org.caijuan.springjpa.dao;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
+import org.caijuan.springjpa.kit.LongListType;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @Accessors(chain = true)
 @Entity
 @Table(name = "users")
+@TypeDef(name = "longListType", typeClass = LongListType.class)
 public class UserDO {
 
     /**
      * 用户编号
      */
+//    @GeneratedValue(strategy = GenerationType.IDENTITY,  // strategy 设置使用数据库主键自增策略；
+//            generator = "JDBC") // generator 设置插入完成后，查询最后生成的 ID 填充到该属性中。
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY,  // strategy 设置使用数据库主键自增策略；
-            generator = "JDBC") // generator 设置插入完成后，查询最后生成的 ID 填充到该属性中。
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     /**
      * 账号
@@ -31,12 +37,17 @@ public class UserDO {
      */
     @Column(nullable = false)
     private String password;
+
+    /**
+     * @OneToMany
+     * https://www.cnblogs.com/h-huakai/p/16574835.html
+     */
+    @Type(type = "longListType")
+    @Column(columnDefinition = "int[]", name = "ids", nullable = false)
+    private List<Long> ids;
     /**
      * 创建时间
      */
     @Column(name = "create_time", nullable = false)
     private Date createTime;
-
-    // ... 省略 setting/getting 方法
-
 }
--- spring-jpa/src/test/java/org/caijuan/springjpa/UserMapperTest.java
+++ spring-jpa/src/test/java/org/caijuan/springjpa/UserMapperTest.java
@@ -1,6 +1,7 @@
-package org.caijuan.springjpa;// UserRepository01.java
+package org.caijuan.springjpa;
 
 import org.caijuan.springjpa.dao.UserDO;
+import org.caijuan.springjpa.dao.UserRepository;
 import org.caijuan.springjpa.dao.UserRepository01;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -15,30 +16,35 @@ import java.util.*;
 public class UserMapperTest {
 
     @Autowired
-    private UserRepository01 userMapper;
+//    private UserRepository userRepository;
+    private UserRepository01 userRepository;
 
     @Test
     public void testInsert() {
+        List<Long> ids = new ArrayList<>();
+        ids.add(10L);
+        ids.add(13L);
+        ids.add(14L);
         UserDO user = new UserDO().setId(1).setUsername(UUID.randomUUID().toString())
-                .setPassword("nicai").setCreateTime(new Date());
-        userMapper.save(user);
+                .setPassword("nicai").setCreateTime(new Date()).setIds(ids);
+        userRepository.save(user);
     }
 
 
     @Test
     public void testSelectById() {
-        Optional<UserDO> optional = userMapper.findById(1);
+        Optional<UserDO> optional = userRepository.findById(1);
         System.out.println(optional.isPresent() ? optional.get() : "NULL");
     }
 
 /*    @Test
     public void testSelectByUsername() {
-        userMapper.selectByUsername("yunai");
+        userRepository.selectByUsername("yunai");
     }*/
 
     @Test
     public void testSelectByIds() {
-        Iterable<UserDO> userDOS = userMapper.findAllById(Arrays.asList(1, 3));
+        Iterable<UserDO> userDOS = userRepository.findAllById(Arrays.asList(1, 3));
         List<UserDO> users = new ArrayList<>();
         userDOS.forEach((users::add));
         System.out.println("users：" + users.size());
@@ -46,7 +52,7 @@ public class UserMapperTest {
 
     @Test
     public void testDeleteById() {
-        userMapper.deleteById(1);
+        userRepository.deleteById(1);
     }
 
 }
