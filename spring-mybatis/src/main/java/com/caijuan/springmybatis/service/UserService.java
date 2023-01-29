5a77d9e155683ebe11a3d676a44f75ddef115462
caiming01 <caiming01@qianxin.com>
2023-01-29 17:22:18 +0800

    测试 mybatis-plus and or

--- spring-mybatis/src/main/java/com/caijuan/springmybatis/service/UserService.java
+++ spring-mybatis/src/main/java/com/caijuan/springmybatis/service/UserService.java
@@ -1,10 +1,12 @@
 package com.caijuan.springmybatis.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.caijuan.springmybatis.mapper.UserMapper;
 import com.caijuan.springmybatis.model.User;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -17,5 +19,16 @@ public class UserService {
 
     public User selectById(long userId){
         return userMapper.selectById(userId);
+    }
+
+    public List<User> test(){
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+
+        queryWrapper.and(wrapper -> wrapper.eq("email","123@qq.com")
+                .or().eq("user_name","张三"));
+
+        queryWrapper.eq("age",22);
+
+        return userMapper.selectList(queryWrapper);
     }
 }
